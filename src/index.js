e858ce1e6cbfd3d8cb12cf848713fa3d18e40d9b
tuppercm <tup319@gmail.com>
2024-07-02 19:08:45 -0400

    Added javascript
    Adds poem content in Typewriter style when form button is selected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    cursor: "",
+    delay: 5,
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
