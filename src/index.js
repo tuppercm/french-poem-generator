67fe1c046a2aabdca1d5fbb231fe5861a873cbcc
tuppercm <tup319@gmail.com>
2024-07-07 14:28:01 -0400

    Updated Context
    Added clearer instruction to make the poem itself 4 lines and add the Signature as a 5th line

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "bb1ff32oa1215694c28tecb0d1659beb";
   let prompt = `Generate a French poem about ${instructionsInput}`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. At the end of the poem, sign the poem with 'SheCodes AI' inside a strong element. Do not include a poem title.";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. At the end of the poem, in line 5, sign the poem with 'SheCodes AI' inside a strong element. Do not include a poem title.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
